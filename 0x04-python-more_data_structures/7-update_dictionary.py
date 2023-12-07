85bed87199ad3e154abacf72cb73f563c6847c8a
Benjamin <krazy2cub@gmail.com>
2023-12-07 07:42:29 -0800

    upd dict upd

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(my_dict):
-    for i in sorted(my_dict.keys()):
-        print("{}: {}".format(i, my_dict[i]))
-
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict
